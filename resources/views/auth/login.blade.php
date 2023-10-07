37ddbdccacec8cda55bb7f6874a7a87c23d037c4
creativesaiful <saifulislamsapon@gmail.com>
2023-10-07 23:43:01 +0600

    Login registration version 0.1 done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
@@ -9,6 +9,7 @@
         @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
                 {{ session('status') }}
+      
             </div>
         @endif
 
@@ -45,4 +46,145 @@
             </div>
         </form>
     </x-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
+  <title>
+        Expense Tracker
+  </title>
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <!-- Nucleo Icons -->
+  <link href="{{ asset('assets/css/nucleo-icons.css')}}" rel="stylesheet" />
+  <link href="{{ asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ asset('assets/css/material-kit-pro.css')}}" rel="stylesheet" />
+  <!-- Nepcha Analytics (nepcha.com) -->
+  <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+  <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+</head>
+
+<body class="sign-in-basic">
+  <!-- Navbar Transparent -->
+ 
+  <!-- End Navbar -->
+  <div class="page-header align-items-start min-vh-100" style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');" loading="lazy">
+    <span class="mask bg-gradient-dark opacity-6"></span>
+    <div class="container my-auto">
+      <div class="row">
+        <div class="col-lg-4 col-md-8 col-12 mx-auto">
+          <div class="card z-index-0 fadeIn3 fadeInBottom">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+              <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
+                <div class="row mt-3">
+                  <div class="col-2 text-center ms-auto">
+                    <a class="btn btn-link px-3" href="javascript:;">
+                      <i class="fa fa-facebook text-white text-lg"></i>
+                    </a>
+                  </div>
+                  <div class="col-2 text-center px-1">
+                    <a class="btn btn-link px-3" href="javascript:;">
+                      <i class="fa fa-github text-white text-lg"></i>
+                    </a>
+                  </div>
+                  <div class="col-2 text-center me-auto">
+                    <a class="btn btn-link px-3" href="javascript:;">
+                      <i class="fa fa-google text-white text-lg"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+         
+
+           
+
+
+            <div class="card-body">
+                 @error('email')
+                <div class="alert alert-danger text-white">{{ $message }}</div>
+                
+            @enderror
+
+              <form  method="POST" action="{{ route('login') }}" class="text-start">
+                @csrf
+                <div class="input-group input-group-outline my-3">
+                  <label class="form-label">Email</label>
+                  <input type="email" name="email" class="form-control" value="{{ old('email') }}" required >
+                </div>
+                <div class="input-group input-group-outline mb-3">
+                  <label class="form-label">Password</label>
+                  <input type="password" name="password" class="form-control" required>
+                </div>
+
+
+                <div class="form-check form-switch d-flex align-items-center mb-3">
+                  <input class="form-check-input" type="checkbox" name="remember" id="rememberMe" checked>
+                  <label class="form-check-label mb-0 ms-3" for="rememberMe">Remember me</label>
+                </div>
+
+                @if (Route::has('password.request'))
+                <div class="mt-4 text-sm text-center ">
+                   <a href="{{ route('password.request') }}">Forgot your password?</a>
+                  </div>
+
+                  @endif
+
+
+
+                <div class="text-center">
+                  {{-- <button type="button" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button> --}}
+
+                  <input type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2" value="Sign in">
+                </div>
+                <p class="mt-4 text-sm text-center">
+                    <a href="{{route('register')}}"> Don't have an account?</a>
+                </p>
+
+              
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+  </div>
+  <!--   Core JS Files   -->
+  <script src="{{asset('assets/js/core/popper.min.js')}}" type="text/javascript"></script>
+  <script src="{{asset('assets/js/core/bootstrap.min.js')}}" type="text/javascript"></script>
+  <script src="{{asset('assets/js/plugins/perfect-scrollbar.min.js')}}" type="text/javascript"></script>
+  <!--  Plugin for TypedJS, full documentation here: https://github.com/mattboldt/typed.js/ -->
+  <script src="{{asset('assets/js/plugins/typedjs.js')}}"></script>
+  <!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
+  <script src="{{asset('assets/js/plugins/parallax.min.js')}}" type="text/javascript"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="{{asset('assets/js/plugins/nouislider.min.js')}}" type="text/javascript"></script>
+  <!--  Plugin for the blob animation -->
+  <script src="{{asset('assets/js/plugins/anime.min.js')}}" type="text/javascript"></script>
+  <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+  <script src="{{asset('assets/js/material-kit-pro.min.js?v=3.0.4')}}" type="text/javascript"></script>
+</body>
+
+</html>
+
+
+
